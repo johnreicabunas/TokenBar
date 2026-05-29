6edee185eb0a0f7eccf56a598f638f465535e5b7
John Rei Bobis Cabuñas <johnreicabunas@gmail.com>
2026-05-29 17:50:16 +0800

    Initial Commit

---
+++ TokenBar/TokenBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenBarApp.swift
+//  TokenBar
+//
+//  Created by John Rei Cabunas on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TokenBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
